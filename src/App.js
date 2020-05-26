b97c3f588ed049c3fa83ccba98a48817f0b47345
Israel-Laguan <israellaguan@gmail.com>
2020-05-26 14:57:00 -0500

    feat(SETUP): Add router logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      Movie Discover
-    </div>
-  );
-}
+const Home = () => (<p>Home</p>)
+const Movie = () => (<p>Movie</p>)
+const NotFound = () => (<p>Not Found</p>)
+
+const App = () => (
+  <BrowserRouter>
+    <React.Fragment>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/:movieId" component={Movie} exact />
+        <Route component={NotFound} />
+      </Switch>
+    </React.Fragment>
+  </BrowserRouter>
+);
 
 export default App;
