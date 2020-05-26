289b8659b241c79195be54c8f0d920f2c4aad404
Israel-Laguan <israellaguan@gmail.com>
2020-05-26 15:56:41 -0500

    feat(VIEW): Add Movie Components

---
+++ src/View/Movie/MovieInfo.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from "../../config";
+import MovieThumb from "../MovieThumb";
+import "./MovieInfo.css";
+
+const MovieInfo = props => {
+  return (
+    <div
+      className="rmdb-movieinfo"
+      style={{
+        background: props.movie.backdrop_path
+          ? `url('${IMAGE_BASE_URL}${BACKDROP_SIZE}${
+              props.movie.backdrop_path
+            }')`
+          : "#000"
+      }}
+    >
+      <div className="rmdb-movieinfo-content">
+        <div className="rmdb-movieinfo-thumb">
+          <MovieThumb
+            image={
+              props.movie.poster_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.movie.poster_path}`
+                : "./images/no_image.jpg"
+            }
+            clickable={false}
+          />
+          <div className="rmdb-movieinfo-text">
+            <h1>{props.movie.title}</h1>
+            <h3>PLOT</h3>
+            <p>{props.movie.overview}</p>
+            <h3>IMDB RATING</h3>
+            <div className="rmdb-rating">
+              <meter
+                min="0"
+                max="100"
+                optimum="100"
+                low="40"
+                high="70"
+                value={props.movie.vote_average * 10}
+              />
+              <p className="rmdb-score">{props.movie.vote_average}</p>
+            </div>
+            {props.directors.length > 1 ? (
+              <h3>DIRECTORS</h3>
+            ) : (
+              <h3>DIRECTOR</h3>
+            )}{" "}
+            {props.directors.map((element, i) => {
+              return (
+                <p key={i} className="rmdb-director">
+                  {element.name}
+                </p>
+              );
+            })}
+          </div>
+          <img alt="" className="fa-film" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieInfo;
---
+++ src/View/Movie/index.js
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+import { API_URL, API_KEY } from "../../config";
+import Navigation from "./Navigation";
+import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
+import Spinner from "../Spinner";
+import "./Movie.css";
+import FourColGrid from "../FourColGrid";
+
+class Movie extends Component {
+  state = {
+    movie: null,
+    actors: null,
+    directors: [],
+    loading: false
+  };
+
+  componentDidMount() {
+    if (localStorage.getItem(`${this.props.match.params.movieId}`)) {
+      const state = JSON.parse(
+        localStorage.getItem(`${this.props.match.params.movieId}`)
+      );
+      this.setState({ ...state });
+    } else {
+      this.setState({ loading: true });
+      //first fetch the movie data and then actors
+      const endpoint = `${API_URL}movie/${
+        this.props.match.params.movieId
+      }?api_key=${API_KEY}&language=en-US`;
+      this.fetchMovieData(endpoint);
+    }
+  }
+
+  fetchMovieData = endpoint => {
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(result => {
+        if (result.status_code) {
+          this.setState({ loading: false });
+        } else {
+          this.setState(
+            {
+              movie: result
+            },
+            () => {
+              const endpoint_credit = `${API_URL}movie/${
+                this.props.match.params.movieId
+              }/credits?api_key=${API_KEY}&language=en-US`;
+              fetch(endpoint_credit)
+                .then(result => result.json())
+                .then(result => {
+                  const directors = result.crew.filter(
+                    member => member.job === "Director"
+                  );
+                  this.setState(
+                    {
+                      actors: result.cast,
+                      directors,
+                      loading: false
+                    },
+                    () => {
+                      localStorage.setItem(
+                        `${this.props.match.params.movieId}`,
+                        JSON.stringify(this.state)
+                      );
+                    }
+                  );
+                });
+            }
+          );
+        }
+      })
+      .catch(error => console.error("Error: ", error));
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.movie ? (
+          <div>
+            <Navigation movie={this.props.location.movieName} />
+            <MovieInfo
+              movie={this.state.movie}
+              directors={this.state.directors}
+            />
+            <MovieInfoBar
+              time={this.state.movie.runtime}
+              budget={this.state.movie.budget}
+              revenue={this.state.movie.revenue}
+            />
+          </div>
+        ) : null}
+
+        {this.state.actors ? (
+          <div className="rmdb-moviegrid">
+            <FourColGrid header={"Actors"}>
+              {this.state.actors.map((element, i) => {
+                return <Actor key={i} actor={element} />;
+              })}
+            </FourColGrid>
+          </div>
+        ) : null}
+
+        {!this.state.actors && !this.state.loading ? (
+          <h1>No Movie Found!</h1>
+        ) : null}
+        {this.state.loading ? <Spinner /> : null}
+      </div>
+    );
+  }
+}
+
+export default Movie;
