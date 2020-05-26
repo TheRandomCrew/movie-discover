a872928b1eb88eded04a54ad61c0c60215db2e97
Israel-Laguan <israellaguan@gmail.com>
2020-05-26 18:17:08 -0500

    fix(): Header arrow function

--- src/View/Header/index.js
+++ src/View/Header/index.js
@@ -2,20 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
-const Header = () => {
-  return (
-    <div className="rmdb-header">
-      <div className="rmdb-header-content">
-        <Link to="/" className="rmdb-logo">
-          <img
-            src="https://img.icons8.com/ultraviolet/40/000000/starred-ticket.png"
-            alt="Movie"
-          />
-          <span>Discover!</span>
-        </Link>
-      </div>
+const Header = () => (
+  <div className="rmdb-header">
+    <div className="rmdb-header-content">
+      <Link to="/" className="rmdb-logo">
+        <img
+          src="https://img.icons8.com/ultraviolet/40/000000/starred-ticket.png"
+          alt="Movie"
+        />
+        <span>Discover!</span>
+      </Link>
     </div>
-  );
-};
+  </div>
+);
 
 export default Header;
